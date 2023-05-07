1efe2a4af5f6896c9b6ff577bb7a061221b20f49
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-05-07 21:22:04 +0930

    basic html format

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,30 @@
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
+
 // All code interacting with DOM runs after browser finished 
 // rendering all elements in HTML
 $(document).ready(function () {
 
 // global variables
-    let clearButton = $("#clear-btn");
+    let city = "";
+
+    // search IDs
+    let citySearch = $("#city-search");
+    let searchBtn = $("#search-btn");
+    let clearBtn = $("#clear-btn");
+    // results IDs
+    let futureForecast = $("future-forecast");
+    let currentCity = $("#current-city");
+    let currentTemp = $("#current-temp");
+    let currentHum = $("#humidity");
+    let currentWS = $("#wind-speed");
     // array for storing past search history (cities)
     let cityHistory = [];
 
@@ -29,6 +50,6 @@ $(document).ready(function () {
   }
 
   // click handlers
-  $("#clear-btn").on("click", clearHistory);
+  clearBtn.on("click", clearHistory);
 
  });
